482cd0b6f35f9a0c6bbfc23baa63133dc7af938c
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-06-17 22:34:29 +0200

    Add driver script to test the tree methods

--- index.js
+++ index.js
@@ -1,6 +1,59 @@
 import { prettyPrint, Tree } from "./tree.js";
 
-const array = [1, 2, 5, 4, 2, 5, 7, 4, 2, 10, 2, 57, 8, 43, 43];
-const tree = new Tree(array);
+const randomArray = () => {
+  const array = [];
+  for (let i = 0; i < Math.floor(Math.random() * 100); i++) {
+    array.push(Math.floor(Math.random() * 100));
+  }
+  return array;
+};
+
+const tree = new Tree(randomArray());
+
+console.log(tree.isBalanced());
+
+const levelOrderArray = [];
+tree.levelOrder((node) => levelOrderArray.push(node.data));
+console.log("Level Order:", levelOrderArray);
+
+const preOrderArray = [];
+tree.preOrder((node) => preOrderArray.push(node.data));
+console.log("Preorder:", preOrderArray);
+
+const inOrderArray = [];
+tree.inOrder((node) => inOrderArray.push(node.data));
+console.log("Inorder:", inOrderArray);
+
+const postOrderArray = [];
+tree.postOrder((node) => postOrderArray.push(node.data));
+console.log("Postorder:", postOrderArray);
+
+tree.insert(101);
+tree.insert(102);
+tree.insert(103);
+tree.insert(104);
+tree.insert(105);
+
+console.log(tree.isBalanced());
+
+tree.rebalance();
+
+console.log(tree.isBalanced());
+
+const levelOrderArray2 = [];
+tree.levelOrder((node) => levelOrderArray.push(node.data));
+console.log("Level Order:", levelOrderArray);
+
+const preOrderArray2 = [];
+tree.preOrder((node) => preOrderArray.push(node.data));
+console.log("Preorder:", preOrderArray);
+
+const inOrderArray2 = [];
+tree.inOrder((node) => inOrderArray.push(node.data));
+console.log("Inorder:", inOrderArray);
+
+const postOrderArray2 = [];
+tree.postOrder((node) => postOrderArray.push(node.data));
+console.log("Postorder:", postOrderArray);
 
 prettyPrint(tree.root);
